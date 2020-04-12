76847be0b879374a10bb04e50cc10805edf5388f
Mackenzie Huber <mackenziehuber99@gmail.com>
2020-04-12 19:14:59 -0400

    E10b

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -6,3 +6,15 @@ func _ready():
 func _unhandled_input(event):
 	if Input.is_action_pressed("Quit"):
 		get_tree().quit()
+
+
+func _on_Timer_timeout():
+	pass # Replace with function body.
+
+
+func _on_Nearby_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_Nearby_body_exited(body):
+	pass # Replace with function body.
